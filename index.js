07bad10b3d9c21d652a1cd144be561cd893bbb1c
KATO Satoshi <s-katou@biglobe.co.jp>
2017-07-26 14:26:56 +0900

    index.js change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
 	var currentTime = new time.Date();
-	currentTime.setTimezone("America/Los_Angels");
+	currentTime.setTimezone("America/Los_Angeles");
 	callback(null, {
 		statusCode: '200',
-		body: 'The time in Los Angels is: ' + currentTime.toString(),
+		body: 'The time in Los Angeles is: ' + currentTime.toString(),
 	});
 };
